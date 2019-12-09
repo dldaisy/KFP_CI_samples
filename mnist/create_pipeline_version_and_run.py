a9a874451b756430ba7b301b7cf153a6ce0434c9
dldaisy <dldaisy@mail.ustc.edu.cn>
2019-12-09 10:49:18 +0800

    mnist 1

---
+++ mnist/create_pipeline_version_and_run.py
@@ -0,0 +1,43 @@
+import kfp
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--version_name', help='Must exist. name of the new version. Must be unique.', type=str)
+parser.add_argument('--run_name', help='name of the new run.', type=str, default='')
+parser.add_argument('--package_url', help='Must exist. pipeline package url', type=str)
+parser.add_argument('--pipeline_id', help = 'Must exist. pipeline id',type=str)
+parser.add_argument('--experiment_id', help = 'experiment id',type=str)
+parser.add_argument('--code_source_url', help = 'url of source code', type=str, default='')
+args = parser.parse_args()
+
+client = kfp.Client()
+
+#create version
+version_body = 
+{"name": args.version_name, 
+"code_source_url": args.code_source_url, 
+"package_url": {"pipeline_url": args.package_url}, 
+"resource_references": [{"key": {"id": args.pipeline_id, "type":3}, "relationship":1}]}
+
+try:
+    response = client.pipelines.create_pipeline_version(body)
+except:
+    return
+
+version_id = response.id
+# create run
+run_name = args.run_name if args.run_name else 'run' + version_id
+resource_references = [{"key": {"id": version_id, "type":4}, "relationship":2}]
+if args.experiment_id:
+    resource_references.append({"key": {"id": args.experiment_id, "type":1}, "relationship": 1})
+run_body={
+    "name":run_name,
+    "resource_references": resource_references
+}
+try:
+    client.runs.create_run(run_body)
+except:
+    pass
+
+
+    
---
+++ mnist/pipeline.py
@@ -0,0 +1,24 @@
+import kfp.dsl as dsl
+from kfp.gcp import use_gcp_secret
+import argparse
+
+# Get commit id to tag image
+parser = argparse.ArgumentParser()
+parser.add_argument('--commit_id', help='Commit Id', type=str)
+args = parser.parse_args()
+
+
+@dsl.pipeline(
+   name='mnist pipeline',
+   description='A pipeline to train a model on mnist dataset and start a tensorboard.'
+)
+def mnist_pipeline():
+   train_step = dsl.ContainerOp(
+       name='train mnist and start tensorboard',
+       image='gcr.io/dldaisy-project/mnist-train:' + args.commit_id
+   ).apply(use_gcp_secret('user-gcp-sa'))
+
+
+if __name__ == '__main__':
+   import kfp.compiler as compiler
+   compiler.Compiler().compile(mnist_pipeline, __file__ + '.zip')
