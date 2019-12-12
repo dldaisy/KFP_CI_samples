0e123b6ccec629d94eef31168ed806d11a0900b9
dldaisy <dldaisy@mail.ustc.edu.cn>
2019-12-12 18:01:30 +0800

    pipeline_spec.parameters type must be a list(repeated in proto). That should be the bug

--- mnist/create_pipeline_version_and_run.py
+++ mnist/create_pipeline_version_and_run.py
@@ -35,13 +35,13 @@ resource_references = [{"key": {"id": version_id, "type":4}, "relationship":2}]
 if args.experiment_id:
     resource_references.append({"key": {"id": args.experiment_id, "type":1}, "relationship": 1})
 run_body={"name":run_name,
-          "pipeline_spec":{"parameters": {"name": "storage_bucket", "value": args.tensorboard_bucket}},
+          "pipeline_spec":{"parameters": [{"name": "storage_bucket", "value": args.tensorboard_bucket}]},
           "resource_references": resource_references}
 print('run body is :{}'.format(run_body))
 try:
     client.runs.create_run(run_body)
 except:
-    pass
+    print('Error Creating Run...')
 
 
 
