05a82665c02e6f3432345125ef3fc61b0c5bc7e6
dldaisy <dldaisy@mail.ustc.edu.cn>
2019-12-12 16:29:54 +0800

    debug info

--- mnist/create_pipeline_version_and_run.py
+++ mnist/create_pipeline_version_and_run.py
@@ -35,9 +35,9 @@ resource_references = [{"key": {"id": version_id, "type":4}, "relationship":2}]
 if args.experiment_id:
     resource_references.append({"key": {"id": args.experiment_id, "type":1}, "relationship": 1})
 run_body={"name":run_name,
-          "pipeline_spec":{"parameters": {"name": "storage_bucket","value": args.tensorboard_bucket}},
+          "pipeline_spec":{"parameters": {"name": "storage_bucket", "value": args.tensorboard_bucket}},
           "resource_references": resource_references}
-
+print('run body is : {}'.format(run_body))
 client.runs.create_run(run_body)
 
 
