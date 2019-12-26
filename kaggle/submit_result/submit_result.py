b5d96f3f4579dfde7e011bd45a030cbcdcd76538
dldaisy <dldaisy@mail.ustc.edu.cn>
2019-12-26 14:19:23 +0800

    debug

--- kaggle/submit_result/submit_result.py
+++ kaggle/submit_result/submit_result.py
@@ -13,12 +13,14 @@ def downoadResult(
     blob.download_to_filename('submisstion.csv')
 
 if __name__ == '__main__':
-    import os
-    os.system("kaggle competitions submit -c house-prices-advanced-regression-techniques -f submission.csv -m 'submit message'")
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--bucket_name', type=str)
     parser.add_argument('--submission_name', type=str)
     args = parser.parse_args()
 
-    submitResult(args.bucket_name, args.submission_name)
+    downoadResult(args.bucket_name, args.submission_name)
+    import os
+    os.system("kaggle competitions submit -c house-prices-advanced-regression-techniques -f submission.csv -m 'submit message'")
+
+    
